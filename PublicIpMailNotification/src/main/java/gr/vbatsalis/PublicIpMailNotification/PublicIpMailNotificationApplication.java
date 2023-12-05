5c00217da8a1fc9e68200f27aa649fd23f265c85
Evangelos Batsalis <vbatsalis@gmail.com>
2023-12-05 22:10:31 +0200

    first commit

---
+++ PublicIpMailNotification/src/main/java/gr/vbatsalis/PublicIpMailNotification/PublicIpMailNotificationApplication.java
@@ -0,0 +1,13 @@
+package gr.vbatsalis.PublicIpMailNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicIpMailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicIpMailNotificationApplication.class, args);
+	}
+
+}
