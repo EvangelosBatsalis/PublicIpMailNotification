25bb91a17631a7d7be36c9aea83c924087f69cd8
Evangelos Batsalis <vbatsalis@gmail.com>
2023-12-06 20:55:34 +0200

    added mail properties

--- PublicIpMailNotification/src/main/java/gr/vbatsalis/PublicIpMailNotification/IpFinderMethod.java
+++ PublicIpMailNotification/src/main/java/gr/vbatsalis/PublicIpMailNotification/IpFinderMethod.java
@@ -16,13 +16,22 @@ import java.net.URL;
 @Component
 public class IpFinderMethod {
     String urlString = "https://checkip.amazonaws.com/";
-    @Scheduled(fixedRate = 5000)
+    String ipVar="0.0.0.0";
+    MailService mailService;
+    @Scheduled(fixedRate = 50000)
     private void getIp() throws Exception{
         try {
             URL url = new URL(urlString); //Created a URL object
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();//open a connection
             try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                System.out.println(br.readLine());
+                //System.out.println(br.readLine());
+                if(!(ipVar.equals(br.readLine()))){
+                    ipVar=br.readLine();
+                    mailService.sentTextEmail(ipVar);
+
+                }
+
+
             } catch (IOException e) { throw new RuntimeException(e);}
         }catch (MalformedURLException e){ throw new RuntimeException("URL is malformed: ",e);
         }catch (IOException e) { throw new RuntimeException(e); }
